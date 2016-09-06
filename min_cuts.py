94af0c4adab17eb01bf2010e9613afbbef3412a8
ana0 <sarah.anne.friend@gmail.com>
2016-09-06 17:17:17 -0400

    Add file io

--- min_cuts.py
+++ min_cuts.py
@@ -2,14 +2,26 @@ import random
 import math
 import copy
 
-test_graph = {'A':['B','G','H'],
-            'B':['A','G','H','C'],
+def create_graph(filename):
+    f = open(filename, 'r')
+    graph = {}
+    # text = f.readline()
+    # print text.split()
+    for line in f: 
+        line = line.split()
+        graph[line[0]] =line[1:]
+    return graph
+
+test_graph = create_graph('MinCutsGraph.txt')
+
+debug_graph = {'1':['B','G','H'],
+            'B':['1','G','H','C'],
             'C':['B','F','D','E'],
             'D':['C','F','E'],
             'E':['C','F','D'],
             'F':['G','C','D','E'],
-            'G':['F','B','A','H'],
-            'H':['B','G','A']}
+            'G':['F','B','1','H'],
+            'H':['B','G','1']}
 
 def repeat(func): 
     def find_limit(args):
@@ -30,13 +42,13 @@ def min_cuts(graph):
         return len(graph.items()[0][1])
     vert = random.choice(graph.keys())
     edge = random.choice(graph[vert]) 
-    graph[vert+edge] = [i for i in graph[vert] + graph[edge] if i != vert 
+    graph[vert+'+'+edge] = [i for i in graph[vert] + graph[edge] if i != vert 
         and i != edge]
     del graph[vert]
     del graph[edge]
-    to_check = set(graph[vert+edge]) 
+    to_check = set(graph[vert+'+'+edge]) 
     for item in to_check:
-        graph[item] = [i if i != vert and i != edge else vert+edge for i in 
+        graph[item] = [i if i != vert and i != edge else vert+'+'+edge for i in 
             graph[item]]
     return min_cuts(graph)
 
@@ -49,5 +61,6 @@ def min_cuts_iter(graph):
     return min(values)
 
 print min_cuts_iter(test_graph)
+# print min_cuts_outer(test_graph)
 
-print min_cuts_outer(test_graph)
+
